650a6d5b8bc079bee28ee186d5c3af6017f4e454
Mino777 <jomino7777@gmail.com>
2022-08-11 01:46:50 +0900

    feat: UITableViewCell Extension 추가

---
+++ GithubUsers/GithubUsers/Common/Extensions/UITableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  UITableViewCell.swift
+//  GithubUsers
+//
+//  Created by 조민호 on 2022/08/10.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
