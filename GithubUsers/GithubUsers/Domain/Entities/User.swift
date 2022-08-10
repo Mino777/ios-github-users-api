1a8da211344e6f587d22279414fd2a7489779cf3
Mino777 <jomino7777@gmail.com>
2022-08-10 23:03:06 +0900

    feat: User Entity 생성

--- GithubUsers/GithubUsers/Domain/Entities/User.swift
+++ GithubUsers/GithubUsers/Domain/Entities/User.swift
@@ -5,6 +5,13 @@
 //  Created by 조민호 on 2022/08/10.
 //
 
+import Foundation
+
 struct User {
-    
+    let login: String
+    let id: Int
+    let avatarURL: String
+    let followersURL: String
+    let followingURL: String
+    let isFollowing: Bool
 }
