f9a2ac9bdb3f893654c0f71a9e5d13211818b5a7
Mino777 <jomino7777@gmail.com>
2022-08-11 16:38:18 +0900

    feat: Network Service 구현
    NetworkService, NetworkServiceError 구현

---
+++ GithubUsers/GithubUsers/Data/Network/Service/NetworkService.swift
@@ -0,0 +1,59 @@
+//
+//  NetworkService.swift
+//  GithubUsers
+//
+//  Created by 조민호 on 2022/08/11.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol NetworkServiceable {
+    func request(endpoint: Endpoint) -> Single<Data>
+}
+
+final class NetworkService: NetworkServiceable {
+    private let urlSession: URLSession
+    
+    init(urlSession: URLSession) {
+        self.urlSession = urlSession
+    }
+    
+    func request(endpoint: Endpoint) -> Single<Data> {
+        return Single<Data>.create { [weak self] single in
+            guard let urlRequest = try? endpoint.create() else {
+                single(.failure(NetworkServiceError.createURLRequestFailure))
+                return Disposables.create()
+            }
+            
+            let task = self?.urlSession.dataTask(with: urlRequest) { data, response, error in
+                guard error == nil, let data = data else {
+                    single(.failure(NetworkServiceError.errorIsOccurred(error.debugDescription)))
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse else {
+                    single(.failure(NetworkServiceError.invalidateResponse))
+                    return
+                }
+                
+                switch response.statusCode {
+                case 200..<300: single(.success(data))
+                case 400: single(.failure(NetworkServiceError.badRequest))
+                case 401: single(.failure(NetworkServiceError.unauthorized))
+                case 404: single(.failure(NetworkServiceError.notFound))
+                case 500: single(.failure(NetworkServiceError.internalServerError))
+                case 503: single(.failure(NetworkServiceError.serviceUnavailable))
+                default: single(.failure(NetworkServiceError.unknown))
+                }
+            }
+            task?.resume()
+            
+            return Disposables.create {
+                task?.suspend()
+                task?.cancel()
+            }
+        }
+    }
+}
